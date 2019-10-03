c714bbad00e63253576681c66963a803a36a4528
Martin Aarhof <martin.aarhof@gmail.com>
2019-10-03 16:37:15 +0200

    initial

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
+];
