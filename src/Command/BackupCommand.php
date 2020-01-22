023ce853dcd965cad47a6697ca93e72311fa04f4
Martin Aarhof <martin.aarhof@gmail.com>
2020-01-22 10:21:52 +0100

    allow single type backup

--- src/Command/BackupCommand.php
+++ src/Command/BackupCommand.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use App\Backup;
 use App\DatabaseBackup;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -34,12 +35,26 @@ class BackupCommand extends Command
     {
         $this
             ->setDescription('Do the backup')
-            ->addOption('no-overwrite', null, InputOption::VALUE_NONE, 'Do not overwrite if file already exists on target');
+            ->addOption('no-overwrite', null, InputOption::VALUE_NONE, 'Do not overwrite if file already exists on target')
+            ->addArgument('type', InputArgument::OPTIONAL, 'Which backup type should be used (file, database)', null);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->databaseBackup->execute($output);
-        $this->backup->execute($output, $input);
+        switch ($input->getArgument('type')) {
+            case 'file':
+                $this->backup->execute($output, $input);
+                break;
+            case 'database':
+                $this->databaseBackup->execute($output);
+                break;
+            case null:
+                $this->databaseBackup->execute($output);
+                $this->backup->execute($output, $input);
+                break;
+            default:
+                $output->writeln('Backup type does not exists');
+        }
+
     }
 }
